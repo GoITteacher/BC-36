657992bf7825b5ceef6555ac91ed608dd61d3586
Пащенко Владимир <pikimelposhta@gmail.com>
2022-10-27 22:33:54 +0300

    'update'

--- src/lesson/oop.js
+++ src/lesson/oop.js
@@ -6,32 +6,6 @@
 
 */
 
-// const baseSalary = 30000;
-// const overtime = 10;
-// const rate = 20;
-
-// const getWage = (baseSalary, overtime, rate) => {
-//   return baseSalary + overtime * rate;
-// };
-
-// getWage(baseSalary, overtime, rate);
-
-// const user1 = {
-//   name: "User",
-//   age: 10,
-//   setName(name) {
-//     this.name = name;
-//   },
-// };
-
-// const user2 = {
-//   name: "User",
-//   age: 10,
-//   setName(name) {
-//     this.name = name;
-//   },
-// };
-
 ////////////////////////////////////////////////
 
 /* 
