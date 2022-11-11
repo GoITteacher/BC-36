796e0e9aa3f6926597ac1d473fa29f96e1c637cb
Пащенко Владимир <pikimelposhta@gmail.com>
2022-11-11 13:19:22 +0200

    3

--- src/js/02-promisification.js
+++ src/js/02-promisification.js
@@ -2,24 +2,24 @@
  * Промисификация:
  * - Функция которая возвращает промис
  */
-function createPromise(delay, value) {
-  let result = 50;
+// function createPromise(delay, value) {
+//   let result = 50;
 
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(value);
-    }, delay);
-  });
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(value);
+//     }, delay);
+//   });
 
-  promise.then(value => {
-    result += value;
-    console.log('Value', result);
-  });
+//   promise.then(value => {
+//     result += value;
+//     console.log('Value', result);
+//   });
 
-  return result;
-}
+//   return result;
+// }
 
-console.log(createPromise(1000, 100));
+// console.log(createPromise(1000, 100));
 /*
  * Промисификация «синхронных» функций
  * - Promise.resolve()
@@ -27,3 +27,27 @@ console.log(createPromise(1000, 100));
  */
 
 // ===========================
+
+// function createPromise(delay, temp) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (temp) resolve(delay);
+//       reject(`Error: ${delay}`);
+//     }, delay);
+//   });
+// }
+
+// const promise1 = createPromise(1000, true);
+// const promise2 = createPromise(1000, true);
+// const promise3 = createPromise(3000, false);
+// const promise4 = createPromise(1000, true);
+
+// const promises = [promise1, promise2, promise3, promise4];
+
+// Promise.any(promises)
+//   .then(value => {
+//     console.log(value);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
