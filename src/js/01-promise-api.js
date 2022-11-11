41bdca15adf4ca75c3b36805268217b091700371
Пащенко Владимир <pikimelposhta@gmail.com>
2022-11-11 10:55:48 +0200

    'update'

--- src/js/01-promise-api.js
+++ src/js/01-promise-api.js
@@ -6,18 +6,48 @@
  *  - Promise.prototype.then(onResolve, onReject)
  */
 
-// function onFulfilled(result) {
-//   console.log('onFulfilled -> onFulfilled');
-//   console.log(`✅ ${result}`);
-// }
+const promise = new Promise((resolve, reject) => {
+  let rand = Math.random();
+  setTimeout(() => {
+    if (rand > 1) {
+      resolve('OK');
+    } else {
+      reject('Hello Wolrd');
+    }
+  }, 1000);
+});
 
-// function onRejected(error) {
-//   console.log('onRejected -> onRejected');
-//   console.log(`❌ ${error}`);
-// }
+function onFulfilled(result) {
+  console.log('onFulfilled -> onFulfilled');
+  console.log(`✅ ${result}`);
+}
+
+function onRejected(error) {
+  console.log('onRejected -> onRejected');
+  console.log(`❌ ${error}`);
+}
 
 /*
  * Цепочки промисов (chaining)
  * Promise.prototype.catch(error)
  * Promise.prototype.finally()
  */
+
+const result = promise
+  .catch(error => {
+    console.log('ERROR');
+    throw new Error('error');
+  })
+  .then(value => {
+    console.log(value);
+    return;
+  })
+  .then(value => {
+    console.log(value);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+  .finally(() => {})
+  .then();
+console.log(result);
--- src/js/02-promisification.js
+++ src/js/02-promisification.js
@@ -2,7 +2,24 @@
  * Промисификация:
  * - Функция которая возвращает промис
  */
+function createPromise(delay, value) {
+  let result = 50;
 
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(value);
+    }, delay);
+  });
+
+  promise.then(value => {
+    result += value;
+    console.log('Value', result);
+  });
+
+  return result;
+}
+
+console.log(createPromise(1000, 100));
 /*
  * Промисификация «синхронных» функций
  * - Promise.resolve()
